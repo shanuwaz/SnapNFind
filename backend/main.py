3d53cb2a909cec05426f7d0a5b36e086a5ee6a4e
shanuwaz <skrraja98@gmail.com>
2025-10-16 00:42:49 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -211,4 +211,4 @@ if os.path.isdir(DATA_DIR):
 # run
 # -----------------------
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=False)
